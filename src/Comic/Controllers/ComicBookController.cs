d0b91ae2ef1f56d95abe9634898dd18bfb76d985
yaronid <yaronid@gmail.com>
2017-03-29 05:48:52 -0700

    viewbag

--- src/Comic/Controllers/ComicBookController.cs
+++ src/Comic/Controllers/ComicBookController.cs
@@ -10,7 +10,18 @@ namespace Comic.Controllers
     {
         public ActionResult detail()
         {
-            
+            ViewBag.Title1 = "spiderman hsamen ";
+            ViewBag.Movienum = 800;
+            ViewBag.Desc = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+                {
+                    "script: dan slott",
+                    "pencils: humberto ramos",
+                    "inks: victor olazaba",
+                    "colors: edgar delgado",
+                    "letters: chris eliopoulos"
+                };
+
             return View();
            
         }
