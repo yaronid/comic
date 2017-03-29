fcb3f739111c107c0fb8055e75757d43080046db
yaronid <yaronid@gmail.com>
2017-03-29 07:25:47 -0700

    add moudle

---
+++ src/Comic/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssuNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favoirie { get; set; }
+    }
+}
