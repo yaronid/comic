00bd36073b33c4602b4f746d7f95978feba38429
yaronid <yaronid@gmail.com>
2017-03-30 04:44:26 -0700

    update view to be strongly typed

--- src/Comic/Controllers/ComicBookController.cs
+++ src/Comic/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,31 @@ namespace Comic.Controllers
     {
         public ActionResult detail()
         {
-            ViewBag.Title1 = "spiderman hsamen ";
-            ViewBag.Movienum = 800;
-            ViewBag.Desc = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-                {
-                    "script: dan slott",
-                    "pencils: humberto ramos",
-                    "inks: victor olazaba",
-                    "colors: edgar delgado",
-                    "letters: chris eliopoulos"
-                };
 
-            return View();
+            //two ways to Initialize object
+            //1.var comicBook = new ComicBook() ; comicBook.SeriesTitle = "";
+            
+            //2.this way...
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "spiderman hsamen",
+                IssuNumber =700,
+                DescriptionHtml= "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist () { Name = "dan slott",Role="script" },
+                    new Artist () { Name = "humberto ramos",Role="pencils" },
+                    new Artist () { Name = "inks: victor",Role="olazaba" },
+                    new Artist () { Name = "edgar delgado",Role="colors" },
+                    new Artist () { Name = "chris eliopoulos",Role="letters" },
+                }
+            
+
+            };
+
+            //2 vways to pass data to views -- dynamic ViewBag.ComicBook = comicBook;
+            //or .. 
+            return View(comicBook);
            
         }
     }
